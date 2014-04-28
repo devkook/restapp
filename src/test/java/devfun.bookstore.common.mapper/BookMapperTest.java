f804d7435fc50d20250e8ea88654b342f38dcf90
devkook <devinkook@gmail.com>
2014-04-28 21:06:35 +0900

    ide 관련 폴더 삭제

--- src/test/java/devfun.bookstore.common.mapper/BookMapperTest.java
+++ src/test/java/devfun.bookstore.common.mapper/BookMapperTest.java
@@ -1,8 +1,7 @@
 package devfun.bookstore.common.mapper;
 
-import static org.junit.Assert.*;
-import java.util.Date;
-import java.util.List;
+import devfun.bookstore.common.config.AppConfig;
+import devfun.bookstore.common.domain.Book;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -10,8 +9,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import devfun.bookstore.common.config.AppConfig;
-import devfun.bookstore.common.domain.Book;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes={AppConfig.class})
